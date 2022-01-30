6bd05684a5c9ed19c2e6a868266577a903497a18
Pirmsho <d.pirmisashvili@gmail.com>
2022-01-30 20:12:03 +0100

    init redux-toolkit

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,22 +1,34 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { useDispatch } from 'react-redux'
+import { addToCart } from '../store/cart/cartSlice'
 import { productType } from '../utils/productType'
 
 import styles from './Product.module.scss'
 
 const Product = (props: productType) => {
+  const dispatch = useDispatch()
   return (
     <article className={styles.product}>
       <Link to={`/${props.id}`}>
         <h1>{props.title}</h1>
       </Link>
-      <Link to="/:product">
+      <Link to={`/${props.id}`}>
         <div className={styles.img_container}>
           <img src={props.image} alt="Product-Thumbnail" />
         </div>
       </Link>
-      <p>Price: {props.price}$</p>
+      <p>
+        Price: <strong>{props.price}$</strong>
+      </p>
+      <p>
+        Category:{' '}
+        <strong>
+          <Link to={`/categories/${props.category}`}>{props.category}</Link>
+        </strong>
+      </p>
       <p>{props.description}</p>
+      <button>+ Add To Cart</button>
     </article>
   )
 }
